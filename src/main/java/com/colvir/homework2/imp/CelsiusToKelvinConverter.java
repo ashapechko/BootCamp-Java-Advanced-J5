fca4d2356732b11a667638b0a261b619985bec72
ashapechko <ashapechko@gmail.com>
2025-04-15 18:50:50 +0300

    homework 2

---
+++ src/main/java/com/colvir/homework2/imp/CelsiusToKelvinConverter.java
@@ -0,0 +1,14 @@
+package com.colvir.homework2.imp;
+
+import com.colvir.homework2.TemperatureConverter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class CelsiusToKelvinConverter implements TemperatureConverter {
+    @Override
+    public double convert(double value) {
+        return value + 273.15;
+    }
+}
