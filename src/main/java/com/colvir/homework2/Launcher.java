913cc59c450e2aa8a4ca2e5fff398da35faea347
ashapechko <ashapechko@gmail.com>
2025-04-15 20:53:14 +0300

    homework 2 update

--- src/main/java/com/colvir/homework2/Launcher.java
+++ src/main/java/com/colvir/homework2/Launcher.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Launcher {
     public static void main(String[] args) {
         try(AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.colvir.homework2")) {
-            TemperatureConverter celsiusToFahrenheitConverter = (TemperatureConverter) applicationContext.getBean("celsiusToFahrenheitConverter");
-            TemperatureConverter fahrenheitToCelsiusConverter = (TemperatureConverter) applicationContext.getBean("fahrenheitToCelsiusConverter");
-            TemperatureConverter celsiusToKelvinConverter = (TemperatureConverter) applicationContext.getBean("celsiusToKelvinConverter");
-            TemperatureConverter kelvinToCelsiusConverter = (TemperatureConverter) applicationContext.getBean("kelvinToCelsiusConverter");
+            TemperatureConverter celsiusToFahrenheitConverter = applicationContext.getBean("celsiusToFahrenheitConverter", TemperatureConverter.class);
+            TemperatureConverter fahrenheitToCelsiusConverter = applicationContext.getBean("fahrenheitToCelsiusConverter", TemperatureConverter.class);
+            TemperatureConverter celsiusToKelvinConverter = applicationContext.getBean("celsiusToKelvinConverter", TemperatureConverter.class);
+            TemperatureConverter kelvinToCelsiusConverter = applicationContext.getBean("kelvinToCelsiusConverter", TemperatureConverter.class);
             System.out.println(celsiusToFahrenheitConverter.convert(100));
             System.out.println(fahrenheitToCelsiusConverter.convert(212));
             System.out.println(celsiusToKelvinConverter.convert(100));
--- src/main/java/com/colvir/homework2/imp/FahrenheitToCelsiusConverter.java
+++ src/main/java/com/colvir/homework2/imp/FahrenheitToCelsiusConverter.java
@@ -1,11 +1,9 @@
 package com.colvir.homework2.imp;
 
 import com.colvir.homework2.TemperatureConverter;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("singleton")
 public class FahrenheitToCelsiusConverter implements TemperatureConverter {
     @Override
     public double convert(double value) {
